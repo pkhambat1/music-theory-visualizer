8d44d3216a25db1f4f073e564cf673f5a05cc2b3
Pezanne Khambatta <39662781+pkhambat1@users.noreply.github.com>
2024-12-14 00:35:07 -0800

    delete useless files

--- react-app/src/utils/generateOctaves.js
+++
@@ -1,4 +0,0 @@
-export const generateOctaves = (octaveCount, baseScale) =>
-  Array.from({ length: octaveCount }, (_, i) => i + 1).flatMap((octave) =>
-    baseScale.map((note) => (note === "C" ? note + octave : note))
-  );
